cf0a0e2f5ddf0ec12277043eebc44273d15465aa
Boyan <104321637+BoyanDashev@users.noreply.github.com>
2024-02-08 21:34:48 +0200

    Creating my first shadow component

--- src/index.js
+++ src/index.js
@@ -1,43 +1,81 @@
 import './style.css';
 
 function component() {
-  const element = document.createElement('div');
+  const element = document.getElementById("root");
+  const shadowRoot = element.attachShadow({ mode: "open" });
 
-  element.innerHTML = 'Hello World!';
   
+  // Set inner HTML content of the shadow DOM
+  shadowRoot.innerHTML = `<div contenteditable="true"><p>Editor</p>  </div>`;
+
+  let hello = document.createElement("span");
+  hello.textContent = "Hi I am shadowed but wrapped in span";
+
+  shadowRoot.appendChild(hello);
 
   return element;
 }
 
-document.body.appendChild(component());
+component();
 
-//<div contenteditable="true"> </div>
 
-  init();
+// let el = document.querySelector(".dom");
+// el.attachShadow({ mode: "open" });
+// // Just like prototype & constructor bi-directional references, we have...
+// el.shadowRoot; // the shadow root.
+// el.shadowRoot.host; // the element itself.
 
-  function init() {
-    // The element
-    let el = document.querySelector(".element");
+// // Put something in shadow DOM
+// el.shadowRoot.innerHTML = "Hi I am shadowed!";
 
-    // Attaching Shadow DOM to the element
-    el.attachShadow({ mode: "open" });
+// // Like any other normal DOM operation.
+// let hello = document.createElement("span");
+// hello.textContent = "Hi I am shadowed but wrapped in span";
+// el.shadowRoot.appendChild(hello);
 
-    // At this point, open Devtools, inspect the element
-    // and expand the element.
+// let el = document.querySelector(".dom");
+// el.attachShadow({ mode: "open" });
+// // Just like prototype & constructor bi-directional references, we have...
+// el.shadowRoot; // the shadow root.
+// el.shadowRoot.host; // the element itself.
 
-    // You'll see the shadow root as in Elements panel as...
-    // <div class="element">
-    //   #shadow-root (open)
-    // </div>
+// // Put something in shadow DOM
+// el.shadowRoot.innerHTML = "Hi I am shadowed!";
 
-    // Now, head over to the console.
-    console.log("Hello! I am the Element - `el` :", el);
-    console.log(
-      "Hello! I am the Shadow Root - `el.shadowRoot` :",
-      el.shadowRoot
-    );
-    console.log(
-      "Hello! I am the Element, again! - `el.shadowRoot.host` :",
-      el.shadowRoot.host
-    );
-  }
+// // Like any other normal DOM operation.
+// let hello = document.createElement("span");
+// hello.textContent = "Hi I am shadowed but wrapped in span";
+// el.shadowRoot.appendChild(hello);
+
+// document.body.appendChild(component());
+
+// //<div contenteditable="true"> </div>
+
+//   init();
+
+//   function init() {
+//     // The element
+//     let el = document.querySelector(".element");
+
+//     // Attaching Shadow DOM to the element
+//     el.attachShadow({ mode: "open" });
+
+//     // At this point, open Devtools, inspect the element
+//     // and expand the element.
+
+//     // You'll see the shadow root as in Elements panel as...
+//     // <div class="element">
+//     //   #shadow-root (open)
+//     // </div>
+
+//     // Now, head over to the console.
+//     console.log("Hello! I am the Element - `el` :", el);
+//     console.log(
+//       "Hello! I am the Shadow Root - `el.shadowRoot` :",
+//       el.shadowRoot
+//     );
+//     console.log(
+//       "Hello! I am the Element, again! - `el.shadowRoot.host` :",
+//       el.shadowRoot.host
+//     );
+//   }
